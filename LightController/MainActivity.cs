9405c4e3ff1a89f13eaecd670eb3eb2853869a19
TherryDing2014 <dingtao_wy@163.com>
2018-03-02 16:13:47 +0800

    test

---
+++ LightController/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LightController
+{
+    [Activity(Label = "LightController", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button btnLight;
+        private bool lightOn = false;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            btnLight = FindViewById<Button>(Resource.Id.btnLight);
+            btnLight.Click += delegate {
+                if (!lightOn) {
+                    btnLight.Text = "On";
+                    lightOn = true;
+                }
+                else{
+                    btnLight.Text = "Off";
+                    lightOn = false;
+                }
+                
+            };
+        }
+    }
+}
+
---
+++ LightController/Protocol/ControllerMsg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LightController.Protocol
+{
+    class ControllerMsg
+    {
+        public short deviceType;        //设备类型
+        public short deviceIndex;       //设备编号
+        public 
+    }
+}
